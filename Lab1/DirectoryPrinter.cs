42bf2593974d84c444fb8c563b77645a09b75609
Pridz <wojtas.p@hotmail.com>
2017-04-22 23:26:08 +0200

    Second commit. Idea is changed. DirectoryPrinter class created. First scratches.

---
+++ Lab1/DirectoryPrinter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class DirectoryPrinter
+    {
+        private String path;
+
+        public DirectoryPrinter(String path)
+        {
+            this.path = path;
+        }
+        private bool isDirectory(String path)
+        {
+            return System.IO.Directory.Exists(path);
+        }
+
+        private bool isFile(String path)
+        {
+            return System.IO.File.Exists(path);
+        }
+
+        public void printDirectory(String path)
+        {
+            if (isDirectory(path))
+            {
+
+            }
+            else if (isFile(path))
+            {
+
+            }
+        }
+
+        public void print()
+        {
+
+        }
+    }
+}
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -10,6 +10,20 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            Directory dir;
+            Console.WriteLine("Write path to directory to print it's interior.");
+            String path = Console.ReadLine();
+            dir = new Directory(path);
+            if (System.IO.Directory.Exists(path))
+            {
+
+            }
+            if (System.IO.File.Exists(path))
+            {
+
+            }
         }
     }
+
+
 }
