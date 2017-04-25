a3700037f59886f2e908bd6fefa6246ed1980b96
Pridz <wojtas.p@hotmail.com>
2017-04-25 13:18:11 +0200

    Extension of DirectoryInfo class. Finding the oldest element in the given directory and subdirectories.

---
+++ Lab1/DirectoryInfoExtension.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public static class DirectoryInfoExtension
+    {
+        public static void findTheOldestFile(this System.IO.DirectoryInfo dirInfo)
+        {
+            System.IO.DirectoryInfo[] dirs = dirInfo.GetDirectories();
+            System.IO.DirectoryInfo theOldestDirectory = null;
+            if (dirs.Length != 0)
+            {
+                theOldestDirectory = dirs[0];
+                foreach (System.IO.DirectoryInfo directory in dirs)
+                {
+                    if (theOldestDirectory.CreationTime > directory.CreationTime)
+                    {
+                        theOldestDirectory = directory;
+                    }
+                    findTheOldestFile(directory);
+                }
+
+            }
+            System.IO.FileInfo[] files = dirInfo.GetFiles();
+            System.IO.FileInfo theOldestFile = null;
+            if (files.Length != 0)
+            {
+                theOldestFile = files[0];
+                foreach (System.IO.FileInfo file in files)
+                {
+                    if (theOldestFile.CreationTime > file.CreationTime)
+                    {
+                        theOldestFile = file;
+                    }
+                }
+            }
+            if (theOldestDirectory != null && theOldestFile != null)
+            {
+                if (theOldestDirectory.CreationTime > theOldestFile.CreationTime)
+                {
+                    Console.WriteLine(theOldestFile.Name);
+                }
+                else
+                {
+                    Console.WriteLine(theOldestDirectory.Name);
+                }
+            }
+            else if (theOldestDirectory == null && theOldestFile != null)
+            {
+                Console.WriteLine(theOldestFile.Name);
+            }
+            else if (theOldestDirectory != null && theOldestFile == null)
+            {
+                Console.WriteLine(theOldestDirectory.Name);
+            }
+        }        
+    }
+}
