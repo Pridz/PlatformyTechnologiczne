0683966aa1b79da242ad5afdc61dcfd1cbd0522b
Pridz <wojtas.p@hotmail.com>
2017-04-25 22:35:46 +0200

    IComparer class Sorter added. Class with sorted dictionary and with populating it implemented.

---
+++ Lab1/ElementsCollection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab1
+{
+    class ElementsCollection
+    {
+        private string path;
+        SortedDictionary<string, int> dictionary;
+
+        public ElementsCollection(string path)
+        {
+            this.path = path;
+            dictionary = new SortedDictionary<string, int>( new Sorter());
+        }
+
+        public void addToDictionary(string nameOfElement, int sizeOfElement)
+        {
+            dictionary.Add(nameOfElement, sizeOfElement);
+        }
+
+        public void addElementsFromPath()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            DirectoryInfo[] paths = dirInfo.GetDirectories();
+            FileInfo[] files = dirInfo.GetFiles();            
+            foreach (DirectoryInfo dir in paths)
+            {
+                addToDictionary(dir.Name, countElements(dir));
+            }
+            foreach (FileInfo file in files)
+            {
+                addToDictionary(file.Name, (int)file.Length);
+            }
+        }
+
+        private int countElements(DirectoryInfo path)
+        {
+            DirectoryInfo[] paths = path.GetDirectories();
+            FileInfo[] files = path.GetFiles();
+            int filesCount = paths.Length + files.Length;
+            return filesCount;
+        }
+    }
+}
---
+++ Lab1/Sorter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Sorter : IComparer<string>
+    {
+        public int Compare(string x, string y )
+        {
+            if (x.Length < y.Length)
+            {
+                return 1;
+            }
+            else if (x.Length == y.Length)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
