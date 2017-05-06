faf93794b1310d81546501605aec1c9507c305cc
Pridz <wojtas.p@hotmail.com>
2017-05-06 19:58:31 +0200

    Deleteing folders and files done through ContextMenu.

--- Lab2/MainWindow.xaml.cs
+++ Lab2/MainWindow.xaml.cs
@@ -60,13 +60,7 @@ namespace Lab2
             treeView.Items.Add(root);
             DirectoryInfo[] dirsInfo = dirInfo.GetDirectories();
             foreach (var dir in dirsInfo)
-            {
-                var item = new TreeViewItem
-                {
-                    ContextMenu = (System.Windows.Controls.ContextMenu)this.FindResource("cmDirTreeView"),
-                    Header = dir.Name,
-                    Tag = dir.FullName
-                };
+            {                
                 makeSubTree(dir, root);
             }
             FileInfo[] filesInfo = dirInfo.GetFiles();
@@ -93,14 +87,7 @@ namespace Lab2
             parent.Items.Add(root);
             DirectoryInfo[] dirsInfo = dirInfo.GetDirectories();
             foreach (var dir in dirsInfo)
-            {
-                var item = new TreeViewItem
-                {
-                    ContextMenu = (System.Windows.Controls.ContextMenu)this.FindResource("cmDirTreeView"),
-                    Header = dir.Name,
-                    Tag = dir.FullName
-                };
-                root.Items.Add(item);
+            {                
                 makeSubTree(dir, root);
             }
             FileInfo[] filesInfo = dirInfo.GetFiles();
@@ -118,7 +105,93 @@ namespace Lab2
 
         private void cmDirCreate_Click(object sender, RoutedEventArgs e)
         {
+            
+        }
+
+        private void cmDirDelete_Click(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item1 = findItem((TreeViewItem)treeView.Items.GetItemAt(0));
+            TreeViewItem parent = (TreeViewItem)item1.Parent;            
+            if (item1.Items.Count != 0)
+            {
+                deleteInteriorOfDirectory(item1);
+            }            
+            parent.Items.Remove(item1);
+            Directory.Delete(item1.Tag.ToString());
+            parent.Items.Refresh();
+            parent.UpdateLayout();
+            treeView.UpdateLayout();
+        }
+
+        private void cmFileDelete_Click(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)treeView.SelectedItem;
+            TreeViewItem item1 = findItem((TreeViewItem)treeView.Items.GetItemAt(0));
+            //TreeViewItem parent = findItemsParent((TreeViewItem)treeView.Items.GetItemAt(0));
+            TreeViewItem parent =  (TreeViewItem)item1.Parent;
+            parent.Items.Remove(item1);
+            File.Delete(item.Tag.ToString());
+            parent.Items.Refresh();
+            parent.UpdateLayout();
+            treeView.UpdateLayout();
+            //treeView.Items.Refresh();
+            //treeView.UpdateLayout();
+            //treeView.Items.Clear();
+        }
+
+        private void deleteInteriorOfDirectory(TreeViewItem item)
+        {
+            while (item.Items.Count != 0)
+            {
+                TreeViewItem node = (TreeViewItem)item.Items.GetItemAt(0);
+                if (Directory.Exists(node.Tag.ToString()))
+                {
+                    deleteInteriorOfDirectory(node);
+                    item.Items.Remove(node);
+                    Directory.Delete(node.Tag.ToString());
+                }
+                else if (File.Exists(node.Tag.ToString()))
+                {
+                    item.Items.Remove(node);
+                    File.Delete(node.Tag.ToString());
+                }
+            }
+        }
+
+        private void printItems()
+        {
 
         }
+
+        private TreeViewItem findItem(TreeViewItem item)
+        {
+            if (File.Exists(item.Tag.ToString()))
+            {
+                if (isSelectedItem(item.Tag.ToString(), (TreeViewItem)treeView.SelectedItem))
+                {
+                return item;
+                }
+            }
+            else if (Directory.Exists(item.Tag.ToString()))
+            {
+                if (isSelectedItem(item.Tag.ToString(), (TreeViewItem)treeView.SelectedItem))
+                {
+                    return item;
+                }
+                for (int i = 0; i < item.Items.Count; i++)
+                {
+                    TreeViewItem result = findItem((TreeViewItem)item.Items.GetItemAt(i));
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private bool isSelectedItem(string path, TreeViewItem item) { return ((string)item.Tag) == path; }
+
+        
     }
 }
